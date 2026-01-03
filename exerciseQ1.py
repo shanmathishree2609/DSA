2f92f664df4a5dbb6bea9602f1e54f99b642d959
shanmathishree2609 <shanmathishree2007@gmail.com>
2026-01-03 10:25:10 -0800

    exerciseQ1.py

---
+++ exerciseQ1.py
@@ -0,0 +1,9 @@
+age = 18
+height = 165
+name = "Shanmathi_shree"
+are_you_student = True
+print("About_me")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", are_you_student)
