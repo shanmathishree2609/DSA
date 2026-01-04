a747a7cc652127523a32513adb619638d58dd54c
shanmathishree2609 <shanmathishree2007@gmail.com>
2026-01-03 22:45:57 -0800

    exerciseQ10.py

--- exerciseQ10.py
+++ exerciseQ10.py
@@ -1,15 +1,11 @@
-# Ask the user for temperature in Celsius
 celsius = float(input("Enter the temperature in Celsius: "))
-# Convert Celsius to Fahrenheit
 fahrenheit = celsius * 9/5 + 32
-# Print the temperature in Fahrenheit
 print(f"Temperature in Fahrenheit: {fahrenheit}°F")
-# Give advice based on Celsius temperature
 if celsius < 0:
     print("Very cold! Wear thick jacket")
 elif 0 <= celsius <= 15:
     print("Cold. Wear jacket")
 elif 16 <= celsius <= 25:
     print("Nice weather")
-else:  # celsius > 25
+else:  
     print("Hot! Stay hydrated")
